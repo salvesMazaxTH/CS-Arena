4fe9c64c1e1db2584d94d8b425a7eb988d9b5c13
Thiago <mazaxbr@gmail.com>
2026-08-19 14:34:44 -0300

    Sabrina baseStats corrected

--- shared/data/champions/sabrina/data.js
+++ shared/data/champions/sabrina/data.js
@@ -4,10 +4,9 @@ export default {
 
   HP: 300,
   Attack: 315,
-  Defense: 58,
-  Speed: 88,
-  Critical: 15,
-
+  Defense: 85,
+  Speed: 75,
+  
   elementalAffinities: ["water", "ice"],
   classKey: "mage",
   species: ["human"],
