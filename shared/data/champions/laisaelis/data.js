d2f99ce48e6ad5d8c51a01d8f2e26e3ab626f69d
Thiago <mazaxbr@gmail.com>
2026-09-01 14:40:59 -0300

    feat(newChar): laisaelis & laiserisa released

--- shared/data/champions/laisaelis/data.js
+++ shared/data/champions/laisaelis/data.js
@@ -1,7 +1,6 @@
 export default {
   name: "Laisaelis",
   portrait: "/assets/portraits/laisaelis.webp",
-  unreleased: true,
   hiddenFromDraftGrid: true,
 
   // Four stats at 250: a thousand split evenly, the whole of one thing.
--- shared/data/champions/laiserisa/data.js
+++ shared/data/champions/laiserisa/data.js
@@ -1,7 +1,6 @@
 export default {
   name: "Laiserisa",
   portrait: "/assets/portraits/laiserisa.webp",
-  unreleased: true,
   hiddenFromDraftGrid: true,
   classKey: "enchanter",
 
