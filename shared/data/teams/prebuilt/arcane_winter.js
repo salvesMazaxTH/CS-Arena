8b59705cb8775d40e8f2d9e3fd3a8fbc65856509
Thiago <mazaxbr@gmail.com>
2026-09-07 19:21:31 -0300

    feat(teams): add the Acane Winter prebuilt, high DPS mages and frost mages with some icy survivability

---
+++ shared/data/teams/prebuilt/arcane_winter.js
@@ -0,0 +1,18 @@
+export const arcaneWinter = {
+  id: "arcane_winter",
+  name: "Arcane Winter",
+  tagline: "A mage line that freezes the target first, behind a frost wall.",
+  origin: "prebuilt",
+  derivedFrom: null,
+  emblems: ["mage_arcana", "permafrost"],
+  champions: [
+    "sabrina",
+    "voltexz",
+    "bruno",
+    "elias_cross",
+    "avarion",
+    "tyren",
+    "nythera",
+    "ysvane",
+  ],
+};
